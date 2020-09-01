d0afda184b4f9f087a34e72670118a70bed1184d
ChennojuBharath <chennojubharath@gmail.com>
2020-09-01 12:49:21 +0530

    commited on 1-09

--- Pages/resources.po.ts
+++ Pages/resources.po.ts
@@ -10,8 +10,15 @@ export class Resources {
         return element(by.xpath("//span[contains(text(),'CREATE NEW RESOURCE')]"));
     }
     Classradiobtn() {
-        return element(by.xpath("//div[@class='mat-radio-label-content'][contains(text(),'Class')]"));
+        return element(by.xpath("//mat-radio-button[@value='Class']"));
     }
+    resourceClassradiobtn() {
+        return element(by.xpath("//p[text()='1']"));
+    }
+     Subjectdropdown() {
+        return element(by.xpath("//select[@name='subjectNames']"));
+    }
+    
     typefile() {
         return element(by.xpath("//input[@type='file']"))
     }
@@ -27,34 +34,30 @@ export class Resources {
         if (dataRecs && dataRecs.length > 0) {
             dataRecs.forEach(record => {
                 browser.sleep(5000);
-                BrowserUtils.scrollIntoView(by.xpath("//a[@ng-reflect-router-link='/resources']/div[contains(text(),'Resources')]"));
+                BrowserUtils.scrollIntoView(by.xpath("//a[@ng-reflect-router-link='/resources']"));
                 this.Resourcelink().click();
                 browser.sleep(2000);
                 //Audio
                 this.Createnewresourcebtn().click();
                 browser.sleep(5000);
-                BrowserUtils.selectDropdownValue(by.xpath("(//select[@class='rv-select w-45 ng-pristine ng-valid ng-touched'])[1]"), record["Class"]);
-                browser.sleep(1500);
-                BrowserUtils.selectDropdownValue(by.xpath("(//select[@class='rv-select w-45 ng-pristine ng-valid ng-touched'])[2]"), record["Type"]);
-                browser.sleep(500);
                 this.Classradiobtn().click();
+                browser.sleep(5000);
+                this.resourceClassradiobtn().click();
                 browser.sleep(500);
-                var classselection = by.xpath("//div[@class='radio-button']//p[contains(text(),'" + record["ResourceClass"] + "')]");
-                browser.sleep(2000);
-                BrowserUtils.clickOnElement(classselection);
                 BrowserUtils.enterText(by.xpath("//input[@ng-reflect-name='title']"), record["AudioTitle"]);
+                 browser.sleep(2000);
                 BrowserUtils.selectDropdownValue(by.xpath("//select[@name='subjectNames']"), record["Subject"]);
+                browser.sleep(2000);
+                BrowserUtils.selectDropdownValue(by.xpath("//select[@name='milestoneNum']"), record["MilestoneNumber"]);
                 browser.sleep(500);
-                // BrowserUtils.selectDropdownValue(by.xpath("//select[@name='milestoneNum']"), record["MilestoneNumber"]);
-                // browser.sleep(500);
                 BrowserUtils.enterText(by.xpath("//textarea[@formcontrolname]"), record["AudioDescription"]);
                 var path1 = '../../TestData/AudioFiles/sample.mp3';
                 var audioPath = path.resolve(__dirname, path1);
                 browser.sleep(500);
                 this.typefile().sendKeys(audioPath);
-                browser.sleep(5000);
+                browser.sleep(10000);
                 this.OKbtn().click();
-                browser.sleep(500);
+                browser.sleep(5000);
                 this.savebtn().click();
                 browser.sleep(5000);
                 this.OKbtn().click();
@@ -62,27 +65,23 @@ export class Resources {
                 //Video
                 this.Createnewresourcebtn().click();
                 browser.sleep(5000);
-                BrowserUtils.selectDropdownValue(by.xpath("(//select[@class='rv-select w-45 ng-pristine ng-valid ng-touched'])[1]"), record["Class"]);
-                browser.sleep(1500);
-                BrowserUtils.selectDropdownValue(by.xpath("(//select[@class='rv-select w-45 ng-pristine ng-valid ng-touched'])[2]"), record["Type"]);
-                browser.sleep(500);
-                this.Classradiobtn().click();
-                browser.sleep(500);
-                var classselection = by.xpath("//div[@class='radio-button']//p[contains(text(),'" + record["ResourceClass"] + "')]");
-                browser.sleep(2000);
-                BrowserUtils.clickOnElement(classselection);
+               this.Classradiobtn().click();
+                browser.sleep(5000);
+                  this.Classradiobtn().click();
+                browser.sleep(5000);
                 BrowserUtils.enterText(by.xpath("//input[@ng-reflect-name='title']"), record["VideoTitle"]);
+                 browser.sleep(2000);
                 BrowserUtils.selectDropdownValue(by.xpath("//select[@name='subjectNames']"), record["Subject"]);
+                 browser.sleep(2000);
+                BrowserUtils.selectDropdownValue(by.xpath("//select[@name='milestoneNum']"), record["MilestoneNumber"]);
                 browser.sleep(500);
-                // BrowserUtils.selectDropdownValue(by.xpath("//select[@name='milestoneNum']"), record["MilestoneNumber"]);
-                // browser.sleep(500);
                 BrowserUtils.enterText(by.xpath("//textarea[@formcontrolname]"), record["VideoDescription"]);
                 var path2 = "../../TestData/VideoFiles/Train.mp4"
                 var videoPath = path.resolve(__dirname, path2);
                 this.typefile().sendKeys(videoPath);
-                browser.sleep(5000);
+                browser.sleep(10000);
                 this.OKbtn().click();
-                browser.sleep(500);
+                browser.sleep(5000);
                 this.savebtn().click();
                 browser.sleep(5000);
                 this.OKbtn().click();
@@ -90,27 +89,23 @@ export class Resources {
                 //PDF
                 this.Createnewresourcebtn().click();
                 browser.sleep(5000);
-                BrowserUtils.selectDropdownValue(by.xpath("(//select[@class='rv-select w-45 ng-pristine ng-valid ng-touched'])[1]"), record["Class"]);
-                browser.sleep(1500);
-                BrowserUtils.selectDropdownValue(by.xpath("(//select[@class='rv-select w-45 ng-pristine ng-valid ng-touched'])[2]"), record["Type"]);
-                browser.sleep(500);
+                 this.Classradiobtn().click();
+                browser.sleep(5000);
                 this.Classradiobtn().click();
-                browser.sleep(500);
-                var classselection = by.xpath("//div[@class='radio-button']//p[contains(text(),'" + record["ResourceClass"] + "')]");
-                browser.sleep(2000);
-                BrowserUtils.clickOnElement(classselection);
+                browser.sleep(5000);
                 BrowserUtils.enterText(by.xpath("//input[@ng-reflect-name='title']"), record["pdfTitle"]);
+                 browser.sleep(2000);
                 BrowserUtils.selectDropdownValue(by.xpath("//select[@name='subjectNames']"), record["Subject"]);
+                 browser.sleep(2000);
+                BrowserUtils.selectDropdownValue(by.xpath("//select[@name='milestoneNum']"), record["MilestoneNumber"]);
                 browser.sleep(500);
-                // BrowserUtils.selectDropdownValue(by.xpath("//select[@name='milestoneNum']"), record["MilestoneNumber"]);
-                // browser.sleep(500);
                 BrowserUtils.enterText(by.xpath("//textarea[@formcontrolname]"), record["pdfDescription"]);
                 var path2 = "../../TestData/PDF/pdf1mb.pdf"
                 var pdfPath = path.resolve(__dirname, path2);
                 this.typefile().sendKeys(pdfPath);
-                browser.sleep(5000);
+                browser.sleep(10000);
                 this.OKbtn().click();
-                browser.sleep(500);
+                browser.sleep(5000);
                 this.savebtn().click();
                 browser.sleep(5000);
                 this.OKbtn().click();
@@ -123,29 +118,31 @@ export class Resources {
         if (dataRecs && dataRecs.length > 0) {
             dataRecs.forEach(record => {
                 browser.sleep(5000);
-                BrowserUtils.scrollIntoView(by.xpath("//a[@ng-reflect-router-link='/resources']/div[contains(text(),'Resources')]"));
+                BrowserUtils.scrollIntoView(by.xpath("//a[@ng-reflect-router-link='/resources']"));
                 this.Resourcelink().click();
                 browser.sleep(2000);
                 //Audio
                 this.Createnewresourcebtn().click();
                 browser.sleep(2000);
-                BrowserUtils.enterText(by.xpath("//input[@ng-reflect-name='title']"), record["Title"]);
-                BrowserUtils.enterText(by.xpath("//textarea[@formcontrolname]"), record["Description"]);
+                BrowserUtils.enterText(by.xpath("//input[@ng-reflect-name='title']"), record["AudioTitle"]);
+                browser.sleep(500);
+                BrowserUtils.enterText(by.xpath("//textarea[@formcontrolname]"), record["AudioDescription"]);
                 var path1 = '../../TestData/AudioFiles/sample.mp3';
                 var audioPath = path.resolve(__dirname, path1);
                 browser.sleep(500);
                 this.typefile().sendKeys(audioPath);
-                browser.sleep(5000);
+                browser.sleep(10000);
                 this.OKbtn().click();
-                browser.sleep(500);
-                this.savebtn().click();
                 browser.sleep(5000);
+                this.savebtn().click();
+                browser.sleep(500);
                 this.OKbtn().click();
                 browser.sleep(1500);
                 //Video
                 this.Createnewresourcebtn().click();
                 browser.sleep(2000);
                 BrowserUtils.enterText(by.xpath("//input[@ng-reflect-name='title']"), record["VideoTitle"]);
+                browser.sleep(500);
                 BrowserUtils.enterText(by.xpath("//textarea[@formcontrolname]"), record["VideoDescription"]);
                 var path2 = "../../TestData/VideoFiles/Train.mp4"
                 var videoPath = path.resolve(__dirname, path2);
@@ -154,20 +151,21 @@ export class Resources {
                 this.OKbtn().click();
                 browser.sleep(500);
                 this.savebtn().click();
-                browser.sleep(5000);
+                browser.sleep(10000);
                 this.OKbtn().click();
-                browser.sleep(1500);
+                browser.sleep(5000);
                 //PDF
                 this.Createnewresourcebtn().click();
                 browser.sleep(2000);
                 BrowserUtils.enterText(by.xpath("//input[@ng-reflect-name='title']"), record["pdfTitle"]);
+                browser.sleep(500);
                 BrowserUtils.enterText(by.xpath("//textarea[@formcontrolname]"), record["pdfDescription"]);
                 var path2 = "../../TestData/PDF/pdf1mb.pdf"
                 var pdfPath = path.resolve(__dirname, path2);
                 this.typefile().sendKeys(pdfPath);
-                browser.sleep(5000);
+                   browser.sleep(10000);
                 this.OKbtn().click();
-                browser.sleep(500);
+                browser.sleep(5000);
                 this.savebtn().click();
                 browser.sleep(5000);
                 this.OKbtn().click();
@@ -181,26 +179,20 @@ export class Resources {
         if (dataRecs && dataRecs.length > 0) {
             dataRecs.forEach(record => {
                 browser.sleep(5000);
-                BrowserUtils.scrollIntoView(by.xpath("//a[@ng-reflect-router-link='/resources']/div[contains(text(),'Resources')]"));
+                BrowserUtils.scrollIntoView(by.xpath("//a[@ng-reflect-router-link='/resources']"));
                 this.Resourcelink().click();
                 browser.sleep(2000);
                 //Audio
                 this.Createnewresourcebtn().click();
                 browser.sleep(5000);
-                BrowserUtils.selectDropdownValue(by.xpath("(//select[@class='rv-select w-45 ng-pristine ng-valid ng-touched'])[1]"), record["Class"]);
-                browser.sleep(1500);
-                BrowserUtils.selectDropdownValue(by.xpath("(//select[@class='rv-select w-45 ng-pristine ng-valid ng-touched'])[2]"), record["Type"]);
-                browser.sleep(500);
                 this.Classradiobtn().click();
-                browser.sleep(500);
-                var classselection = by.xpath("//div[@class='radio-button']//p[contains(text(),'" + record["ResourceClass"] + "')]");
-                browser.sleep(2000);
-                BrowserUtils.clickOnElement(classselection);
+               browser.sleep(2000);
                 BrowserUtils.enterText(by.xpath("//input[@ng-reflect-name='title']"), record["AudioTitle"]);
+                browser.sleep(2000);
                 BrowserUtils.selectDropdownValue(by.xpath("//select[@name='subjectNames']"), record["Subject"]);
+               browser.sleep(2000);
+                BrowserUtils.selectDropdownValue(by.xpath("//select[@name='milestoneNum']"), record["MilestoneNumber"]);
                 browser.sleep(500);
-                // BrowserUtils.selectDropdownValue(by.xpath("//select[@name='milestoneNum']"), record["MilestoneNumber"]);
-                // browser.sleep(500);
                 BrowserUtils.enterText(by.xpath("//textarea[@formcontrolname]"), record["AudioDescription"]);
                 var path1 = '../../TestData/AudioFiles/sample.mp3';
                 var audioPath = path.resolve(__dirname, path1);
@@ -216,20 +208,14 @@ export class Resources {
                 //Video
                 this.Createnewresourcebtn().click();
                 browser.sleep(5000);
-                BrowserUtils.selectDropdownValue(by.xpath("(//select[@class='rv-select w-45 ng-pristine ng-valid ng-touched'])[1]"), record["Class"]);
-                browser.sleep(1500);
-                BrowserUtils.selectDropdownValue(by.xpath("(//select[@class='rv-select w-45 ng-pristine ng-valid ng-touched'])[2]"), record["Type"]);
-                browser.sleep(500);
                 this.Classradiobtn().click();
                 browser.sleep(500);
-                var classselection = by.xpath("//div[@class='radio-button']//p[contains(text(),'" + record["ResourceClass"] + "')]");
-                browser.sleep(2000);
-                BrowserUtils.clickOnElement(classselection);
                 BrowserUtils.enterText(by.xpath("//input[@ng-reflect-name='title']"), record["VideoTitle"]);
+                 browser.sleep(2000);
                 BrowserUtils.selectDropdownValue(by.xpath("//select[@name='subjectNames']"), record["TeluguSubject"]);
+                 browser.sleep(2000);
+                BrowserUtils.selectDropdownValue(by.xpath("//select[@name='milestoneNum']"), record["MilestoneNumber"]);
                 browser.sleep(500);
-                // BrowserUtils.selectDropdownValue(by.xpath("//select[@name='milestoneNum']"), record["MilestoneNumber"]);
-                // browser.sleep(500);
                 BrowserUtils.enterText(by.xpath("//textarea[@formcontrolname]"), record["VideoDescription"]);
                 var path2 = "../../TestData/VideoFiles/Train.mp4"
                 var videoPath = path.resolve(__dirname, path2);
@@ -244,20 +230,14 @@ export class Resources {
                 //PDF
                 this.Createnewresourcebtn().click();
                 browser.sleep(5000);
-                BrowserUtils.selectDropdownValue(by.xpath("(//select[@class='rv-select w-45 ng-pristine ng-valid ng-touched'])[1]"), record["Class"]);
-                browser.sleep(1500);
-                BrowserUtils.selectDropdownValue(by.xpath("(//select[@class='rv-select w-45 ng-pristine ng-valid ng-touched'])[2]"), record["Type"]);
-                browser.sleep(500);
                 this.Classradiobtn().click();
                 browser.sleep(500);
-                var classselection = by.xpath("//div[@class='radio-button']//p[contains(text(),'" + record["ResourceClass"] + "')]");
-                browser.sleep(2000);
-                BrowserUtils.clickOnElement(classselection);
                 BrowserUtils.enterText(by.xpath("//input[@ng-reflect-name='title']"), record["pdfTitle"]);
+             browser.sleep(2000);
                 BrowserUtils.selectDropdownValue(by.xpath("//select[@name='subjectNames']"), record["Subject"]);
+               browser.sleep(2000);
+                BrowserUtils.selectDropdownValue(by.xpath("//select[@name='milestoneNum']"), record["MilestoneNumber"]);
                 browser.sleep(500);
-                // BrowserUtils.selectDropdownValue(by.xpath("//select[@name='milestoneNum']"), record["MilestoneNumber"]);
-                // browser.sleep(500);
                 BrowserUtils.enterText(by.xpath("//textarea[@formcontrolname]"), record["pdfDescription"]);
                 var path2 = "../../TestData/PDF/pdf1mb.pdf"
                 var pdfPath = path.resolve(__dirname, path2);
@@ -277,7 +257,7 @@ export class Resources {
         if (dataRecs && dataRecs.length > 0) {
             dataRecs.forEach(record => {
                 browser.sleep(5000);
-                BrowserUtils.scrollIntoView(by.xpath("//a[@ng-reflect-router-link='/resources']/div[contains(text(),'Resources')]"));
+                BrowserUtils.scrollIntoView(by.xpath("//a[@ng-reflect-router-link='/resources']"));
                 this.Resourcelink().click();
                 browser.sleep(2000);
                 //Audio
@@ -289,7 +269,7 @@ export class Resources {
                 var audioPath = path.resolve(__dirname, path1);
                 browser.sleep(500);
                 this.typefile().sendKeys(audioPath);
-                browser.sleep(5000);
+                browser.sleep(10000);
                 this.OKbtn().click();
                 browser.sleep(500);
                 this.savebtn().click();
